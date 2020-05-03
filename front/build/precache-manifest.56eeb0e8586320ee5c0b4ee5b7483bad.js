b6522363764e9f0c682906f3803771a3ea5de6af
Jellfedora <julienlecointe@live.fr>
2020-05-03 23:10:03 +0200

    Bump to 0.2

---
+++ front/build/precache-manifest.56eeb0e8586320ee5c0b4ee5b7483bad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a1bfc1670e516583a7f36b8d5acb29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fab8172d3e82c6d124e",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "307131509150426be3b9",
+    "url": "/static/css/main.4b6f9d08.chunk.css"
+  },
+  {
+    "revision": "8fab8172d3e82c6d124e",
+    "url": "/static/js/2.6d714751.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d714751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307131509150426be3b9",
+    "url": "/static/js/main.ae8dbc61.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
