87e4f39b18806211fb28a215e5887df70c837755
Jellfedora <julienlecointe@live.fr>
2020-05-11 21:35:01 +0200

    Added a search video bar

--- front/src/components/Video/index.jsx
+++ front/src/components/Video/index.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import Fuse from 'fuse.js'
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const apiUrl = process.env.REACT_APP_REST_API;
+
 
 class Video extends Component {
     constructor(props) {
         super(props);
         this.state = {
             listOfVideos: [],
-            selectVideo: null
+            searchInlistOfVideos: [],
+            titleOfVideos: [],
+            searchVideoResults: [],
+            searchActive: false
         };
     }
 
@@ -29,44 +34,53 @@ class Video extends Component {
             .then(response => {
                 let listOfVideos = this.state.listOfVideos
 
-                console.log("Todo remplacer par foreach")
+
+                let titleOfVideos = this.state.titleOfVideos
+                let test = []
                 response.data.map((item, i) => {
+                    console.log(item)
                     let filmDetail = item;
                     listOfVideos.push(filmDetail);
-                    this.setState({ listOfVideos: listOfVideos })
+
+                    test = {
+                        'key': item.title.toLowerCase(),
+                        'value': item.title,
+                    }
+
+
+                    titleOfVideos.push(test);
+
+                    this.setState({ listOfVideos: listOfVideos, titleOfVideos: titleOfVideos })
                     // return true;
                 })
+                // this.triArray()
             })
             .catch(error => {
             });
     }
 
-    // getVideoInfo = (filmTitle, i) => {
-    //     // Tmdb test
-    //     console.log(filmTitle)
-    //     const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + filmTitle
-    //     axios.get(url
-    //     )
-    //         .then(response => {
-    //             console.log(response)
-    //             let listOfVideos = this.state.listOfVideos
-    //             let filmDetail = {
-    //                 'title': filmTitle,
-    //                 'overview': response.data.results[0].overview,
-    //                 'poster_path': "https://image.tmdb.org/t/p/w300" + response.data.results[0].poster_path,
-    //                 'vote_average': response.data.results[0].vote_average,
-    //             }
-    //             listOfVideos.push(filmDetail)
-    //             this.setState({ listOfVideos: listOfVideos })
-    //         })
-    //         .catch(error => {
-    //         });
+    handleSearchFilmChange = (e) => {
+        let idTarget = e.target.value;
+        this.setState({ searchTerm: idTarget });
 
 
-    // }
+        if (idTarget.length > 0) {
+            const options = {
+                includeScore: true,
+                keys: ['title']
+            }
+            const fuse = new Fuse(this.state.listOfVideos, options)
+            const result = fuse.search(idTarget)
+            this.setState({ searchVideoResults: result, searchActive: true })
 
-    streamVideo = (item) => {
-        this.setState({ selectVideo: item })
+        } else {
+            this.setState({ searchActive: false })
+        }
+
+    }
+
+    deleteSearch = () => {
+        this.setState({ searchVideoResults: [], searchActive: false })
     }
 
     render() {
@@ -82,12 +96,45 @@ class Video extends Component {
                 </Link >
             );
         });
-        // const selectVideo = this.state.selectVideo;
+        const searchVideos = this.state.searchVideoResults.map((item, i) => {
+            return (
+                <Link className="video__content__video-info" to={'/video-detail/' + item.item.title} key={i}>
+                    < img src={item.item.poster_path} alt="film-poster" className="video__content__video-info__img" />
+                    <div className="video__content__video-info__text">
+                        <span className="video__content__video-info__text__title">{item.item.title}</span>
+                        <p className="video__content__video-info__text__overview">{item.item.overview.substr(0, 250)}</p>
+                        <span className="video__content__video-info__text__vote">Note Tmdb: {item.item.vote_average} / 10</span>
+                    </div>
+                </Link >
+            );
+        });
 
         return (
             <div className="video" >
                 <div className="video__content">
-                    {listOfVideos}
+                    {this.state.searchActive
+                        ?
+                        <div className="video__content__display">
+                            {searchVideos}
+                        </div>
+                        :
+                        <div className="video__content__display">
+                            {listOfVideos}
+                        </div>
+                    }
+                </div>
+                <div className="video__searchbar">
+                    <div className='video__searchbar__opacity'>
+                    </div>
+                    <div className='video__searchbar__content'>
+                        <input type="text" id="InputId" value={this.state.searchFilm} onChange={this.handleSearchFilmChange} placeholder="Rechercher" />
+                        <button className='video__searchbar__content__delete' onClick={this.deleteSearch}>
+                            <FontAwesomeIcon
+                                icon="times-circle"
+                                size="1x"
+                            />
+                        </button>
+                    </div>
                 </div>
             </div >
         );
